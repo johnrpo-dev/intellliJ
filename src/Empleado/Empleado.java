437b958b4d81077d31dd1405bd42381d36af11b3
johnrpo-dev <johnsod8729@hotmail.com>
2021-10-31 15:41:18 -0500

    clase empleado

--- src/Empleado/Empleado.java
+++ src/Empleado/Empleado.java
@@ -1,4 +1,31 @@
 package Empleado;
 
 public class Empleado {
+
+    int id ;
+    String name;
+    String cargo;
+    int salary;
+    String telephone;
+    String address;
+
+
+    //metodo especial --------->constructor
+    public Empleado() {
+    }
+
+    public Empleado(int id, String name, String cargo, int salary, String telephone, String address) {
+        this.id = id;
+        this.name = name;
+        this.cargo = cargo;
+        this.salary = salary;
+        this.telephone = telephone;
+        this.address = address;
+    }
+
+    //metodos = acciones = funciones
+    public int calcularSalario (){
+
+        return (0);
+    }
 }
